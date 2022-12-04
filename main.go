33dfa4482517c247c8df11c00923eaeded6f7a30
jonlk <jlkovach01@gmail.com>
2022-12-04 02:11:21 -0500

    code cleanup;

--- main.go
+++ main.go
@@ -14,28 +14,29 @@ func main() {
 
 	url := fmt.Sprintf(`https://api.nal.usda.gov/fdc/v1/food/334147?api_key=%s`, os.Getenv("USDA_API_KEY"))
 
-	req, _ := http.NewRequest("GET", url, nil)
+	if req, err := http.NewRequest("GET", url, nil); err == nil {
 
-	client := &http.Client{}
+		client := &http.Client{}
 
-	if resp, err := client.Do(req); err == nil {
+		if resp, err := client.Do(req); err == nil {
 
-		defer resp.Body.Close()
+			defer resp.Body.Close()
 
-		if body, err := io.ReadAll(resp.Body); err == nil {
+			if body, err := io.ReadAll(resp.Body); err == nil {
 
-			food := food.Food{}
+				food := food.Food{}
 
-			json.Unmarshal([]byte(body), &food)
+				json.Unmarshal([]byte(body), &food)
 
-			fmt.Println(`Results For`, food.Description)
+				fmt.Println(`Results For`, food.Description)
 
-			for _, f := range food.FoodNutrients {
-				if f.Amount > 0 {
-					fmt.Println(f.Nutrient.Name, f.Amount, f.Nutrient.UnitName)
+				for _, f := range food.FoodNutrients {
+					if f.Amount > 0 {
+						fmt.Println(f.Nutrient.Name, f.Amount, f.Nutrient.UnitName)
+					}
 				}
-			}
 
+			}
 		}
 	} else {
 		println(err.Error())
